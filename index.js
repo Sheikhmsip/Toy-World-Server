6e5d1bb47dceda4d3299cc380210a556c84d8ac4
Sheikhmsip <sheikhmsip@gmail.com>
2023-05-21 03:52:54 +0600

    Get single Products

--- index.js
+++ index.js
@@ -40,6 +40,13 @@ app.get('/products', async(req, res) =>{
 })
 
 
+app.get('/products/:id', async(req, res) =>{
+  const id = req.params.id;
+  const query = {_id: new ObjectId(id)}
+  const result = await productsCollection.findOne(query);
+  res.send(result);
+})
+
 
 
 // Add toy 
